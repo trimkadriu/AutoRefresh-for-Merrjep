7edb7755b1e7bfc114fed5141c8dc5b241d48b42
Trim Kadriu <trim.kadriu@asseco-see.com>
2014-10-13 17:09:05 +0200

    Refactoring and Adding C# application for multiple request on multiple URL's

---
+++ C# Application/MainForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Auto_Refresh_for_MerrJep
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
