80e0178068cb97bdf3712f1b6d29724c7aa6aacd
Trim Kadriu <trimkadriu@users.noreply.github.com>
2014-10-15 17:10:18 +0200

    Work on C# App

--- C# Application/MainForm.Designer.cs
+++ C# Application/MainForm.Designer.cs
@@ -36,13 +36,15 @@
             this.dgwList = new System.Windows.Forms.DataGridView();
             this.UrlColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.StatusColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.txtTime = new System.Windows.Forms.NumericUpDown();
             this.label4 = new System.Windows.Forms.Label();
+            this.btnRemoveUrl = new System.Windows.Forms.Button();
+            this.btnServerUrlConf = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgwList)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTime)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -57,25 +59,27 @@
             // txtUrlOfPost
             // 
             this.txtUrlOfPost.Location = new System.Drawing.Point(78, 14);
+            this.txtUrlOfPost.MaxLength = 1024;
             this.txtUrlOfPost.Name = "txtUrlOfPost";
-            this.txtUrlOfPost.Size = new System.Drawing.Size(314, 20);
+            this.txtUrlOfPost.Size = new System.Drawing.Size(321, 20);
             this.txtUrlOfPost.TabIndex = 1;
             // 
             // btnAddUrl
             // 
-            this.btnAddUrl.Location = new System.Drawing.Point(398, 12);
+            this.btnAddUrl.Location = new System.Drawing.Point(405, 12);
             this.btnAddUrl.Name = "btnAddUrl";
             this.btnAddUrl.Size = new System.Drawing.Size(81, 23);
             this.btnAddUrl.TabIndex = 3;
             this.btnAddUrl.Text = "Add in list";
             this.btnAddUrl.UseVisualStyleBackColor = true;
+            this.btnAddUrl.Click += new System.EventHandler(this.btnAddUrl_Click);
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(326, 178);
+            this.btnStart.Location = new System.Drawing.Point(428, 227);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(153, 46);
-            this.btnStart.TabIndex = 4;
+            this.btnStart.Size = new System.Drawing.Size(169, 47);
+            this.btnStart.TabIndex = 9;
             this.btnStart.Text = "Start AutoRefresh !!!";
             this.btnStart.UseVisualStyleBackColor = true;
             // 
@@ -91,11 +95,11 @@
             this.UrlColumn,
             this.StatusColumn});
             this.dgwList.Location = new System.Drawing.Point(15, 41);
-            this.dgwList.MultiSelect = false;
             this.dgwList.Name = "dgwList";
             this.dgwList.ReadOnly = true;
             this.dgwList.RowHeadersVisible = false;
-            this.dgwList.Size = new System.Drawing.Size(464, 127);
+            this.dgwList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgwList.Size = new System.Drawing.Size(582, 177);
             this.dgwList.TabIndex = 5;
             // 
             // UrlColumn
@@ -110,17 +114,19 @@
             this.StatusColumn.Name = "StatusColumn";
             this.StatusColumn.ReadOnly = true;
             // 
-            // textBox1
+            // txtPassword
             // 
-            this.textBox1.Location = new System.Drawing.Point(127, 178);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(182, 20);
-            this.textBox1.TabIndex = 7;
+            this.txtPassword.Location = new System.Drawing.Point(127, 228);
+            this.txtPassword.MaxLength = 64;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '•';
+            this.txtPassword.Size = new System.Drawing.Size(182, 20);
+            this.txtPassword.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 181);
+            this.label2.Location = new System.Drawing.Point(12, 231);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(109, 13);
             this.label2.TabIndex = 6;
@@ -129,49 +135,88 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 206);
+            this.label3.Location = new System.Drawing.Point(12, 256);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(113, 13);
             this.label3.TabIndex = 8;
             this.label3.Text = "Time for Auto Refresh:";
             // 
-            // numericUpDown1
+            // txtTime
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(127, 204);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(147, 20);
-            this.numericUpDown1.TabIndex = 9;
+            this.txtTime.Location = new System.Drawing.Point(127, 254);
+            this.txtTime.Maximum = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
+            this.txtTime.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(147, 20);
+            this.txtTime.TabIndex = 7;
+            this.txtTime.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(280, 206);
+            this.label4.Location = new System.Drawing.Point(280, 256);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(29, 13);
             this.label4.TabIndex = 10;
             this.label4.Text = "(min)";
             // 
+            // btnRemoveUrl
+            // 
+            this.btnRemoveUrl.Location = new System.Drawing.Point(492, 12);
+            this.btnRemoveUrl.Name = "btnRemoveUrl";
+            this.btnRemoveUrl.Size = new System.Drawing.Size(105, 23);
+            this.btnRemoveUrl.TabIndex = 11;
+            this.btnRemoveUrl.Text = "Remove Selected";
+            this.btnRemoveUrl.UseVisualStyleBackColor = true;
+            this.btnRemoveUrl.Click += new System.EventHandler(this.btnRemoveUrl_Click);
+            // 
+            // btnServerUrlConf
+            // 
+            this.btnServerUrlConf.Location = new System.Drawing.Point(315, 227);
+            this.btnServerUrlConf.Name = "btnServerUrlConf";
+            this.btnServerUrlConf.Size = new System.Drawing.Size(107, 47);
+            this.btnServerUrlConf.TabIndex = 13;
+            this.btnServerUrlConf.Text = "API Server URL\r\n[ configure ]";
+            this.btnServerUrlConf.UseVisualStyleBackColor = true;
+            // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(499, 239);
+            this.ClientSize = new System.Drawing.Size(609, 290);
+            this.Controls.Add(this.btnServerUrlConf);
+            this.Controls.Add(this.btnRemoveUrl);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.txtTime);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dgwList);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.btnAddUrl);
             this.Controls.Add(this.txtUrlOfPost);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "mainForm";
-            this.Text = "AutoRefresh for MerrJep © 2014 by Trim Kadriu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AutoRefresh for MerrJep.com © 2014 by Trim Kadriu";
             this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgwList)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTime)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -186,11 +231,13 @@
         private System.Windows.Forms.DataGridView dgwList;
         private System.Windows.Forms.DataGridViewTextBoxColumn UrlColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn StatusColumn;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown txtTime;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRemoveUrl;
+        private System.Windows.Forms.Button btnServerUrlConf;
     }
 }
 
--- C# Application/MainForm.cs
+++ C# Application/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace Auto_Refresh_for_MerrJep
 {
     public partial class mainForm : Form
     {
+        String apiServerUrl;
+
         public mainForm()
         {
             InitializeComponent();
@@ -19,6 +22,53 @@ namespace Auto_Refresh_for_MerrJep
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            
+            String configurationFile = AppDomain.CurrentDomain.BaseDirectory + "conf.ini";
+            if (File.Exists(@configurationFile))
+            {
+                apiServerUrl = "";
+                using (StreamReader sr = File.OpenText(configurationFile))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+            else
+            {
+                APIServerUrlForm apiServerUrlForm = new APIServerUrlForm();
+                apiServerUrlForm.ShowDialog();
+                apiServerUrl = apiServerUrlForm.apiServerUrl;
+                using (FileStream fs = File.Create(configurationFile))
+                {
+                    Byte[] info = new UTF8Encoding(true).GetBytes("ApiServerUrl=" + apiServerUrl);
+                    fs.Write(info, 0, info.Length);
+                }
+            }
+        }
+
+        private void btnAddUrl_Click(object sender, EventArgs e)
+        {
+            String url = txtUrlOfPost.Text;
+            if (String.IsNullOrWhiteSpace(url))
+            {
+                MessageBox.Show(null, "Error! Please fill the URL field", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            int index = dgwList.Rows.Add();
+            DataGridViewRow row = dgwList.Rows[index];
+            row.Cells["UrlColumn"].Value = url;
+            row.Cells["StatusColumn"].Value = "idle";
+        }
+
+        private void btnRemoveUrl_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow item in this.dgwList.SelectedRows)
+            {
+                dgwList.Rows.RemoveAt(item.Index);
+            }
         }
     }
 }
