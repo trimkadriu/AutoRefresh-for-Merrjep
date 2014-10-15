1f8da625b51585f49cfbb2c13762aaf5891d72c0
Trim Kadriu <trim.kadriu@asseco-see.com>
2014-10-15 17:10:18 +0200

    Work on C# App

---
+++ C# Application/AddServerUrl.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Auto_Refresh_for_MerrJep
+{
+    partial class APIServerUrlForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtApiServerUrl = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "API Server URL:";
+            // 
+            // txtApiServerUrl
+            // 
+            this.txtApiServerUrl.Location = new System.Drawing.Point(15, 38);
+            this.txtApiServerUrl.Name = "txtApiServerUrl";
+            this.txtApiServerUrl.Size = new System.Drawing.Size(335, 20);
+            this.txtApiServerUrl.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(275, 73);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // APIServerUrlForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 115);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtApiServerUrl);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "APIServerUrlForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "API Server URL";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.APIServerUrlForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtApiServerUrl;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
---
+++ C# Application/AddServerUrl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Auto_Refresh_for_MerrJep
+{
+    public partial class APIServerUrlForm : Form
+    {
+        public string apiServerUrl { get; set; }
+
+        public APIServerUrlForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(txtApiServerUrl.Text))
+            {
+                MessageBox.Show("Please add a valid URL");
+            }
+            else
+            {
+                apiServerUrl = txtApiServerUrl.Text;
+                this.Close();
+            }
+        }
+
+        private void APIServerUrlForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(txtApiServerUrl.Text))
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
