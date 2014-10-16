14d5179fc3182eb2a51d5e88243ead2706164d51
Trim Kadriu <trimkadriu@users.noreply.github.com>
2014-10-16 03:02:51 +0200

    Work on C# App

--- C# Application/Program.cs
+++ C# Application/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,7 +18,48 @@ namespace Auto_Refresh_for_MerrJep
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new mainForm());
+
+            String configurationFile = AppDomain.CurrentDomain.BaseDirectory + "conf.ini";
+            const String key = "ApiServerUrl=";
+            String apiServerUrl;
+
+            if (File.Exists(@configurationFile))
+            {
+                apiServerUrl = "";
+                using (StreamReader sr = File.OpenText(configurationFile))
+                {
+                    String line = "";
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        apiServerUrl = line;
+                    }
+                }
+                apiServerUrl = apiServerUrl.Substring(apiServerUrl.IndexOf(key) + key.Length);
+                if(String.IsNullOrWhiteSpace(apiServerUrl))
+                {
+                    MessageBox.Show("Invalid configure file. Application will exit.");
+                    return;
+                }
+
+                Application.Run(new MainForm(apiServerUrl));
+            }
+            else
+            {
+                APIServerUrlForm apiServerUrlForm = new APIServerUrlForm(false, null);
+                DialogResult apiServerDialogResult = apiServerUrlForm.ShowDialog();
+                if (apiServerDialogResult.Equals(DialogResult.No))
+                {
+                    return;
+                }
+                apiServerUrl = apiServerUrlForm.apiServerUrl;
+                using (FileStream fs = File.Create(configurationFile))
+                {
+                    Byte[] info = new UTF8Encoding(true).GetBytes(key + apiServerUrl);
+                    fs.Write(info, 0, info.Length);
+                }
+
+                Application.Run(new MainForm(apiServerUrl));
+            }
         }
     }
 }
